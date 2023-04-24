3aaf062e38b3cfe7785d8a0cea7cf8d31b5492fe
SheddieMbuvi <sheddiembuvi@gmail.com>
2023-04-24 03:41:02 -0700

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,24 +7,20 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i = 0, j = 0, k = 0;
 
-	for (; i < size - 1; i++)
-	{
-		for (; j < size - i - 1; j++)
+	size_t i = 0, j = 0, tmp = 0;
+
+	if (size < 2)
+		return;
+	for (; i < size; i++)
+		for (; j < size; j++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[j] > array[j + 1] && array[j + 1])
 			{
-				int temp = array[j];
-
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+			tmp = array[j];
+			array[j] = array[j + 1];
+			array[j + 1] = tmp;
+			print_array(array, size);
 			}
 		}
-		for (; k < size; k++)
-		{
-			printf("%d", array[k]);
-		}
-		printf("\n");
-	}
 }
